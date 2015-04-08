755ddf6c502e6aa026639bd2a4e7d36895442471
Osmo Salomaa <otsaloma@iki.fi>
2015-04-08 20:51:19 +0300

    Add Leaflet.

---
+++ Leaflet.docset/Contents/Resources/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import bs4, os, re, sqlite3
+os.chdir(os.path.dirname(__file__))
+if os.path.isfile("docSet.dsidx"):
+    os.remove("docSet.dsidx")
+db = sqlite3.connect("docSet.dsidx")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")
+cursor.execute("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);")
+root = "http://leafletjs.com/reference.html"
+for fname in ("reference.html",):
+    path = os.path.join("Documents", fname)
+    page = open(path, encoding="utf_8", errors="ignore").read()
+    soup = bs4.BeautifulSoup(page)
+    for tag in soup.find_all("tr", {"id": re.compile("-")}):
+        name = tag["id"].strip().replace("-", ".")
+        if not name: continue
+        if re.search(r"\s", name): continue
+        path = root + "#" + tag["id"].strip()
+        print("{}: {}".format(name, path))
+        cursor.execute("INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)",
+                       (name, "func", path))
+
+db.commit()
+db.close()
