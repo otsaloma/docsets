1a251d4666d39edb53fe61044545241a7f9f5d13
Osmo Salomaa <otsaloma@iki.fi>
2023-01-06 01:59:52 +0200

    Fix root URL

--- Requests.docset/Contents/Resources/index.py
+++ Requests.docset/Contents/Resources/index.py
@@ -3,7 +3,7 @@
 import util
 
 db = util.create_database()
-root = "https://docs.python-requests.org/en/master/api/"
+root = "https://requests.readthedocs.io/en/latest/api/"
 for soup in util.soups_from_files("Documents/*.html"):
     for tag in soup.select('dt[id^="requests."]'):
         name = tag.attrs["id"]
