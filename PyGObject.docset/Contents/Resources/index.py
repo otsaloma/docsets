9915c70da6785ee1859daa3b5f228c763203bd0a
Osmo Salomaa <otsaloma@iki.fi>
2023-01-06 19:41:24 +0200

    Use double quotes

--- PyGObject.docset/Contents/Resources/index.py
+++ PyGObject.docset/Contents/Resources/index.py
@@ -4,10 +4,10 @@ import util
 
 db = util.create_database()
 for soup in util.soups_from_files("Documents/*.html"):
-    tag = soup.select_one('.shortcuts .wy-breadcrumbs a')
+    tag = soup.select_one(".shortcuts .wy-breadcrumbs a")
     module = tag.text.replace(" ", "-")
     root = f"https://lazka.github.io/pgi-docs/{module}/"
-    for tag in soup.select('a.reference.internal'):
+    for tag in soup.select("a.reference.internal"):
         if not "title" in tag.attrs: continue
         name = tag.attrs["title"]
         path = root + tag.attrs["href"]
