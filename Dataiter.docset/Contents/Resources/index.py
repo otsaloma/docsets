b853521f6548fd14125a610b8ec8d8148edfa36b
Osmo Salomaa <otsaloma@iki.fi>
2023-08-03 18:04:08 +0300

    Use stable after all for Dataiter

--- Dataiter.docset/Contents/Resources/index.py
+++ Dataiter.docset/Contents/Resources/index.py
@@ -3,7 +3,7 @@
 import util
 
 db = util.create_database()
-root = "https://dataiter.readthedocs.io/en/latest/"
+root = "https://dataiter.readthedocs.io/en/stable/"
 for soup in util.soups_from_files("Documents/*.html"):
     for tag in soup.select('a[href*="#dataiter."]'):
         name = tag.attrs["href"].split("#")[-1]
