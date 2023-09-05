0e749c624f081f5e7ad9528d4dd33ee591cba111
Osmo Salomaa <otsaloma@iki.fi>
2023-09-05 22:37:27 +0300

    Dataiter: use latest for now

--- Dataiter.docset/Contents/Resources/index.py
+++ Dataiter.docset/Contents/Resources/index.py
@@ -3,7 +3,7 @@
 import util
 
 db = util.create_database()
-root = "https://dataiter.readthedocs.io/en/stable/"
+root = "https://dataiter.readthedocs.io/en/latest/"
 for soup in util.soups_from_files("Documents/*.html"):
     for tag in soup.select('a[href*="#dataiter."]'):
         name = tag.attrs["href"].split("#")[-1]
