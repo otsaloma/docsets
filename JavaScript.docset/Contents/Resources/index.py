bf840b303a8299d95608a6f4324b229a1644139f
Osmo Salomaa <osmo.salomaa@futurice.com>
2026-03-16 17:44:41 +0200

    Fix JavaScript

--- JavaScript.docset/Contents/Resources/index.py
+++ JavaScript.docset/Contents/Resources/index.py
@@ -7,6 +7,7 @@ root = "https://developer.mozilla.org"
 for soup in util.soups_from_files("Documents/*.html"):
     for tag in soup.select('a[href*="/en-US/docs/Web/JavaScript/Reference/"]'):
         name = tag.text
+        if not name: continue
         if " " in name: continue
         if not name[0].isalpha(): continue
         path = root + tag.attrs["href"]
