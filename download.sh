fa744ae68dedaf279ce6e0922d908b0d388a3d3b
Osmo Salomaa <otsaloma@iki.fi>
2026-06-20 02:01:57 +0300

    Try to avoid some download issues

--- download.sh
+++ download.sh
@@ -13,6 +13,7 @@ download () {
          --max-time 30 \
          --output Documents/$HASH.html \
          --silent \
+         --user-agent "Mozilla/5.0 Gecko/20100101 Firefox/134.0" \
          "$URL"
 
     test -s Documents/$HASH.html
@@ -25,6 +26,8 @@ for DIRECTORY; do
     cd "$ROOT_DIR/$DIRECTORY/Contents/Resources"
     test -f URLS || continue
     mkdir -p Documents
-    cat URLS | sort -R | parallel -j8 --halt now,fail=1 download {#} {} || echo "$DIRECTORY" >> "$FAIL_FILE"
+    JOBS=$(awk 'END {j=int(NR/10); print (j<1?1:j>8?8:j)}' URLS)
+    echo "Downloading $(wc -l URLS) using $JOBS jobs..."
+    cat URLS | sort -R | parallel -j$JOBS --halt now,fail=1 download {#} {} || echo "$DIRECTORY" >> "$FAIL_FILE"
 done
 echo; sed 's/^/DOWNLOAD FAILED FOR /' "$FAIL_FILE"
